2111b23c7f186e5f0fc8f57cfb7a3ea1ff669639
Nikola Divic <divicnikola@gmail.com>
2017-05-04 15:51:50 +0200

    Configuration from file and tornado application factory
    Replaced hardcoded conf variables with reading from a conf file
    and I setup an application factory so we can launch multiple tornado
    servers on multiple ports. Deleted some leftover junk files.

--- testme.py
+++ testme.py
@@ -2,18 +2,19 @@ from syncer.interactors import Blockchain, BlockchainSyncer
 from syncer.gateways import MongoDatabaseGateway
 from pymongo import MongoClient
 from bitcoinrpc.authproxy import AuthServiceProxy
+import ConfigParser
 import redis
 
 REDIS_CLIENT = redis.Redis()
-RPC_USER = "62ca2d89-6d4a-44bd-8334-fa63ce26a1a3"
-RPC_PASSWORD = "CsNa2vGB7b6BWUzN7ibfGuHbNBC1UJYZvXoebtTt1eup"
+
+config = ConfigParser.RawConfigParser()
+config.read('/home/vagrant/.exploder/exploder.conf')
 
 client = MongoClient()
-database = MongoDatabaseGateway(client.exploder)
-blockchain = Blockchain(database)
+database = MongoDatabaseGateway(client.exploder, config)
+blockchain = Blockchain(database, config)
 
-blocks_dir = "/home/vagrant/.gamecredits/blocks"
 rpc_client = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"
-                              % (RPC_USER, RPC_PASSWORD))
-syncer = BlockchainSyncer(database, blockchain, blocks_dir, rpc_client)
+                              % (config.get('syncer', 'rpc_user'), config.get('syncer', 'rpc_password')))
+syncer = BlockchainSyncer(database, blockchain, rpc_client, config)
 syncer.sync_auto()
