5ceb6dd0d27f806ae696b85c9ac7ebad12775714
Vuksan Simunović <vuk@gamecredits.net>
2017-06-02 14:10:36 +0200

    Add casting to float just in case if freegeoip.net api changes

--- syncer/interactors.py
+++ syncer/interactors.py
@@ -359,8 +359,8 @@ class BlockchainAnalyzer(object):
                 response = requests.get("%s/%s/%s" % (self.config.get('syncer', 'geo_ip_url'), "json", ip))
                 json_data = json.loads(response.text)
                 if json_data and json_data['latitude'] and json_data['longitude']:
-                    peer['latitude'] = json_data['latitude']
-                    peer['longitude'] = json_data['longitude']
+                    peer['latitude'] = float(json_data['latitude'])
+                    peer['longitude'] = float(json_data['longitude'])
         return peer_info
 
     def save_client_info(self, version, ip, peer_info):
