01e198b545589929e3c17dbb204a032df4ebc4e8
jovancucic <jovan.cucic@gamecredits.com>
2018-03-02 15:54:52 +0100

    Added boolean for main chain checking

--- exploder_api/api.py
+++ exploder_api/api.py
@@ -97,9 +97,16 @@ def get_transaction_confirmations(txid):
         return "Transaction with given txid not found", 404
 
     block = db.get_block_by_hash(tr['blockhash'])
+
+    if block['chain'] == 'main_chain':
+        main_chain = True
+    else:
+        main_chain = False
+
     return {
         "txid": txid,
-        "confirmations": db.calculate_block_confirmations(block, rpc)
+        "confirmations": db.calculate_block_confirmations(block, rpc),
+        "mainChain": main_chain
     }
 
 
